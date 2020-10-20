c1622b30078054a1b108a5f00ac1288cd981d627
ardus-uk <peter@ardus.co.uk>
2020-10-20 13:33:29 +0100

    Add more sophisticated templates

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,15 @@ from app import app
 @app.route('/index')
 
 def index():
-    user = {'username': 'Peter'}
-    return render_template('index.html', title='Home', user=user)  
-
-@app.route('/hello')
-
-def hello():
-    user = {'username': 'Ruth'}
-    return render_template('hello.html', title='Home', user=user)  
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
