8c35437ef36f4c7961a3acf2cf58b15453b5f7ae
ardus-uk <peter@ardus.co.uk>
2020-10-21 17:27:39 +0100

    Reduce to max 79 chars per line

--- app/routes.py
+++ app/routes.py
@@ -19,11 +19,13 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        f_username = form.username.data
+        f_remember_me = form.remember_me.data
+        flash('Login requested for user {}, remember_me={}'.format(f_username, f_remember_me))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
