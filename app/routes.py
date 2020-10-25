f44e49e4ba48b1b81d382d06cf371eda601a4df6
ardus-uk <peter@ardus.co.uk>
2020-10-25 16:08:53 +0000

    Amend routes.py for LoginForm

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,6 @@ from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Admin'}
     posts = [
@@ -20,7 +19,6 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
-
 def login():
     form = LoginForm()
     if form.validate_on_submit():
